55d424297e8ea31b32bd7a17d17a0f8852e11a97
it24101617 <it24101617@my.sliit.lk>
2025-09-19 00:14:51 +0530

    IT24101617.R

---
+++ Lab Sheet 06/IT24101617_Lab 6.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\VICTUS\\Desktop\\IT24101617")
+#binomial distribution
+pbinom(47,50,0.85,lower.tail = FALSE)
+1-pbinom(47,50,0.85,lower.tail = TRUE)
+#number of customer calls per hour
+#poisson distribution
+#number of customer calls per hour
+#poisson distribution
+dpois(15,12)
